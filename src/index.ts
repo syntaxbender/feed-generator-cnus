93b380c97f3221bc4beb6c394a3c42781a014e45
Junco <juncolockleader@github>
2024-11-17 21:45:10 -0800

    update env

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const run = async () => {
   dotenv.config()
   const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'blueskycn.club'
   const serviceDid =
-    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:plc:${hostname}`
+    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
   const server = FeedGenerator.create({
     port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
     listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
